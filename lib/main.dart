606eecc1cd031244fb2d5fc94ac3722c9b06e010
YorranPetrick <104874842+YorranPetrick@users.noreply.github.com>
2023-07-13 22:02:25 -0300

    Blue Rabbit Bookstore
    Creating a Login Page for a Blue Rabbit Name Bookstore

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          title: const Text(
+            'Blue rabbit',
+            style: TextStyle(fontSize: 23),
+          ),
+          actions: [
+            // Criando uma lista de Icons
+            IconButton(
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.help),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Image(
+                  image: const AssetImage('Image/Livro.jpg'),
+                ),
+                SizedBox(
+                  width: 300, //Vai alterar a largura de forma automática
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: ElevatedButton(
+                    child: const Text(
+                      'Accounts',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
